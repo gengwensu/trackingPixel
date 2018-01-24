e073b721924858ffee36b82529347e198db9acb9
Gary Su <gengwensu@yahoo.com>
2018-01-24 11:18:42 -0500

    code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # trackingPixel
 This service should be implemented by a customer adding the following tag "<img src=“http//stores.example.com/ tracking/track.gif”></img>” to each of their product pages, where those product pages are marked up with Open Graph tags and where stores.example.com is a unique domain for that customer that points to our multi-tenant SaaS environment.  Assuming that all requests for /tracking/track.gif are already routed to the service, the service needs to respond to the request with a 1px by 1px transparent gif as quickly as possible.  The service also needs to scrape the page that requested the tracking pixel for all open graph markup such that the scraped data can be shown to an admin user at some point in the future.  Assume that there is no urgency for the admin user to see the data.  Do not worry about how the data is eventually presented to the user or how they get access to it, only that it needs to be stored in such a way that an application independent of this service will be able to access it and present it to the user.
+
+# environment & build
+ require Go
+
+ $ go get github.com/dyatlov/go-opengraph/opengraph
+ 
+ $ go build ../src/github.com/gengwensu/trackingPixel/trackingPixel.go
+ $./trackingPixel -outfile="result.txt" &
---
+++ trackingPixel.go
@@ -0,0 +1,92 @@
+/*
+trackingPixel: an OpenGraph data scrapper.
+*/
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"flag"
+
+	"github.com/dyatlov/go-opengraph/opengraph"
+)
+
+//AppContext contains all global variables
+type AppContext struct {
+	Cache       map[string]int // cache for url
+	FileHandler *os.File       // file handler
+}
+
+const base64GifPixel = "R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+
+func main() {
+	outFile := flag.String("outfile", "scrap.txt", "output file for scrape results")
+	flag.Parse()
+	urlCache := map[string]int{}
+	
+	//Open file for append
+	f, err := os.OpenFile(*outFile, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	globalVar := AppContext{
+		FileHandler: f,
+		Cache:       urlCache,
+	}
+
+	log.Fatal(http.ListenAndServe("localhost:8081", &globalVar))
+}
+
+func (ds *AppContext) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/tracking", "/tracking/": // for monitoring purpose
+		if req.Method == "GET" {
+			fmt.Fprint(w, "TrackingPixel service\n") // return signature of the service
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+
+	case "/tracking/track.gif":
+		if req.Method == "GET" {
+			output, _ := base64.StdEncoding.DecodeString(base64GifPixel)
+			w.Header().Set("Content-Type", "image/gif")
+			w.Write(output) // return with a 1x1 transparent gif
+			defer req.Body.Close()
+			// now take care of the scrapping
+			urlString := req.Header.Get("Referer") //get the Referer's url
+			if _, ok := ds.Cache[urlString]; ok {  //url in cache
+				ds.Cache[urlString]++
+			} else { // not in cache
+				ds.Cache[urlString]++                 //cache it
+				go scrapOG(urlString, ds.FileHandler) // run the scrapper in a go routine
+			}
+		} else {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		}
+
+	default:
+		w.WriteHeader(http.StatusNotFound) // 404
+	}
+}
+
+func scrapOG(url string, f *os.File) {
+	resp, _ := http.Get(url)
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	og := opengraph.NewOpenGraph()
+	err := og.ProcessHTML(strings.NewReader(string(bytes)))
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		_, _ = f.WriteString(url + " : ")
+		_, _ = f.WriteString(og.String())
+		_, _ = f.WriteString(" ***** ")
+	}
+}
